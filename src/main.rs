7668284c7e1dd531bba8dcd9fb39fbf8ccfae975
Peter Engelbert <pmengelbert@gmail.com>
2022-05-07 12:06:35 -0500

    Revert "Tried using Transport wrapper but it's too opinionated in its validation"
    This reverts commit 77111ab546693bba2b622ba0da2e03d5053de8b8.

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,20 @@
 use std::io::prelude::*;
-use std::io::Cursor;
 use std::os::unix::net::UnixStream;
-
-struct Sock(UnixStream);
-
-impl xmlrpc::Transport for Sock {
-    type Stream = Cursor<Vec<u8>>;
-    fn transmit(
-        self,
-        request: &xmlrpc::Request<'_>,
-    ) -> Result<Self::Stream, Box<dyn std::error::Error + Send + Sync>> {
-        let msg = wrap_xml_request(request);
-        let mut sock = self.0;
-
-        sock.write_all(&msg)?;
-
-        let mut resp = Vec::new();
-        sock.read_to_end(&mut resp)?;
-
-        Ok(Cursor::new(resp))
-    }
-}
 
 fn main() -> std::io::Result<()> {
     let cmd = std::env::args().nth(1).expect("need an arg");
     let x = xmlrpc::Request::new(&cmd);
+    let msg = wrap_xml_request(x);
 
-    let s = x
-        .call(Sock(UnixStream::connect("/var/run/rtorrent/rpc.socket")?))
-        .unwrap();
+    dbg!("1");
+    let mut stream = UnixStream::connect("/var/run/rtorrent/rpc.socket")?;
+    stream.write_all(&msg)?;
 
-    let mut v = Vec::new();
-    s.write_as_xml(&mut v)?;
-    let s = String::from_utf8(v).unwrap();
+    dbg!("2");
+    let mut response = String::new();
+    stream.read_to_string(&mut response)?;
 
-    // for arg in std::env::args().skip(2) {
-    //     x = x.arg(arg);
-    // }
-
-    // let msg = wrap_xml_request(&x);
-
-    // dbg!("1");
-    // let mut stream = UnixStream::connect("/var/run/rtorrent/rpc.socket")?;
-    // stream.write_all(&msg)?;
-
-    // dbg!("2");
-    // let mut response = String::new();
-    // stream.read_to_string(&mut response)?;
-
-    println!("{}", s);
+    println!("{}", response);
     Ok(())
 }
 
@@ -60,7 +26,7 @@ fn add_header(v: &mut Vec<u8>, key: &str, value: &str) {
     v.push(0);
 }
 
-fn wrap_xml_request(req: &xmlrpc::Request) -> Vec<u8> {
+fn wrap_xml_request(req: xmlrpc::Request) -> Vec<u8> {
     let mut xml = vec![];
     req.write_as_xml(&mut xml).unwrap();
 
