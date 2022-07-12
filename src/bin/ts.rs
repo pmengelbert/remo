c247ee0e01d8d9ddb6f912e8c5ea7002e45d123c
Peter Engelbert <pmengelbert@gmail.com>
2022-07-12 11:59:40 -0500

    Refactor
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- src/bin/ts.rs
+++ src/bin/ts.rs
@@ -30,6 +30,30 @@ struct Channel {
     channel: Vec<Item>,
 }
 
+struct Options {
+    season: String,
+    episode: String,
+    download_first: bool,
+    print_all: bool,
+    help: bool,
+    print_num: usize,
+    loose: Vec<String>,
+}
+
+impl std::default::Default for Options {
+    fn default() -> Self {
+        Self {
+            season: String::default(),
+            episode: String::default(),
+            loose: Vec::default(),
+            download_first: false,
+            print_all: false,
+            help: false,
+            print_num: 5,
+        }
+    }
+}
+
 fn main() -> Result<()> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() == 1 {
@@ -37,88 +61,85 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
-    let mut opt = getopt::Parser::new(&args, "has:e:1n:d");
     let api_key = std::env::var("JACKETT_API_KEY").unwrap_or_else(|_| "password".to_owned());
+    let Options {
+        help,
+        print_all,
+        download_first,
+        season,
+        episode,
+        mut print_num,
+        loose,
+    } = parse_options(&args, "ha1s:e:n:")?;
 
-    let mut season = String::new();
-    let mut episode = String::new();
-    let mut download_first = false;
-    let mut num = 5_usize;
-    let mut print_all = false;
-
-    while let Some(o) = opt.next().transpose()? {
-        match o {
-            getopt::Opt('h', None) => {
-                print_help();
-                return Ok(());
-            }
-            getopt::Opt('s', Some(s)) => season = s.clone(),
-            getopt::Opt('e', Some(e)) => episode = e.clone(),
-            getopt::Opt('n', Some(n)) => num = n.parse::<usize>()?,
-            getopt::Opt('1', None) => download_first = true,
-            getopt::Opt('a', None) => print_all = true,
-            getopt::Opt('*', _) => (),
-            _ => unreachable!(),
-        }
-    }
-
-    let loose = opt.loose;
-    let search_term: String = loose.join("+");
-    let mut url = format!(
-        "http://10.0.0.3:30333/api/v2.0/indexers/all/results/torznab?apikey={}&t=search&q={}",
-        api_key, search_term
-    );
-
-    let is_tv_show = !season.is_empty() || !episode.is_empty();
-    if is_tv_show {
-        if season.is_empty() {
-            return Err(anyhow::format_err!(
-                "if -e is provided, -s must also be provided"
-            ));
-        }
-
-        url = format!("http://10.0.0.3:30333/api/v2.0/indexers/all/results/torznab?apikey={}&t=tvsearch&q={}&season={}", api_key, search_term, season);
-        if !episode.is_empty() {
-            url.push_str("&ep=");
-            url.push_str(&episode);
-        }
-    }
-
-    let doc = reqwest::blocking::get(&url)?.text()?;
-
-    let doc: Channel = serde_xml_rs::from_str(&doc).unwrap_or_default();
-    if doc.channel.len() == 0 {
-        let s = format!("no torrents found for search term {:?}", &loose);
-        return if download_first {
-            Err(anyhow::format_err!("{}", s))
-        } else {
-            Ok(())
-        };
-    }
-
-    if download_first {
-        call_rpc(&["load.start", "", &doc.channel[0].item[0].link])?;
+    if help {
+        print_help();
         return Ok(());
     }
 
-    let mut count = 0;
-    let mut seeders = String::new();
-
-    num = if print_all {
-        doc.channel[0].item.len()
-    } else {
-        std::cmp::min(doc.channel[0].item.len(), num)
+    let search_term: String = loose.join("+");
+    let base_url = format!(
+        "http://10.0.0.3:30333/api/v2.0/indexers/all/results/torznab?apikey={}",
+        api_key
+    );
+    let url = match &(season.len(), episode.len()) {
+        (0, 0) => format!("{}&t=search&q={}", base_url, search_term),
+        (1.., 0) => format!(
+            "{}&t=tvsearch&q={}&season={}",
+            base_url, search_term, season
+        ),
+        (1.., 1..) => format!(
+            "{}&t=tvsearch&q={}&season={}&ep={}",
+            base_url, search_term, season, episode
+        ),
+        (0, 1..) => {
+            return Err(anyhow::format_err!(
+                "if -e is provided, -s must also be provided"
+            ))
+        }
+        (_, _) => unreachable!(),
     };
 
-    for t in &doc.channel[0].item[..num] {
+    let c = reqwest::blocking::Client::new();
+    let doc = c.get(&url).send()?.text()?;
+    let doc: Channel = serde_xml_rs::from_str(&doc).unwrap_or_default();
+
+    if doc.channel.len() == 0 {
+        let s = format!("no torrents found for search term {:?}", &loose);
+        if download_first {
+            return Err(anyhow::format_err!("{}", s));
+        }
+
+        return Ok(());
+    }
+
+    let channel = &doc.channel[0];
+
+    if download_first {
+        let item = &channel.item[0];
+        call_rpc(&["load.start", "", &item.link])?;
+        println!("{}", notify(&c, &item.title)?);
+        return Ok(());
+    }
+
+    print_num = if print_all {
+        channel.item.len()
+    } else {
+        std::cmp::min(channel.item.len(), print_num)
+    };
+
+    let mut count = 0;
+
+    for t in &channel.item[..print_num] {
         let size = t.size.parse::<u64>()?;
         let size = parse_size(size)?;
 
-        for attr in &t.attr {
-            if attr.name == "seeders" {
-                seeders = attr.value.clone();
-            }
-        }
+        let seeders = t
+            .attr
+            .iter()
+            .find(|a| a.name == "seeders")
+            .map(|a| a.value.as_str())
+            .unwrap_or_default();
 
         println!(
             "{})\tname:\t\t{}\n\tseeders:\t{}\n\tsize:\t\t{}\n\tindexer:\t{}\n",
@@ -134,17 +155,44 @@ fn main() -> Result<()> {
     std::io::stdin().read_line(&mut s)?;
     let i: usize = s.trim().parse()?;
 
-    call_rpc(&["load.start", "", &doc.channel[0].item[i].link])?;
-
-    let c = reqwest::blocking::Client::new();
-    let resp = c
-        .request(reqwest::Method::POST, "http://10.0.0.3:30777/")
-        .body(doc.channel[0].item[i].title.clone())
-        .send()?;
-
-    println!("{}", resp.text()?);
+    let item = &channel.item[i];
+    call_rpc(&["load.start", "", &item.link])?;
+    println!("{}", notify(&c, &item.title)?);
 
     Ok(())
+}
+
+fn notify(c: &reqwest::blocking::Client, msg: &str) -> Result<String> {
+    let resp = c
+        .request(reqwest::Method::POST, "http://10.0.0.3:30777/")
+        .body(msg.to_owned())
+        .send()?;
+
+    Ok(resp.text()?)
+}
+
+fn parse_options(args: &[String], getopt: &'static str) -> Result<Options> {
+    let mut opt = getopt::Parser::new(&args, getopt);
+    let mut opts = Options::default();
+
+    while let Some(o) = opt.next().transpose()? {
+        match o {
+            getopt::Opt('h', None) => {
+                opts.help = true;
+                return Ok(opts);
+            }
+            getopt::Opt('s', Some(s)) => opts.season = s.clone(),
+            getopt::Opt('e', Some(e)) => opts.episode = e.clone(),
+            getopt::Opt('n', Some(n)) => opts.print_num = n.parse::<usize>()?,
+            getopt::Opt('1', None) => opts.download_first = true,
+            getopt::Opt('a', None) => opts.print_all = true,
+            getopt::Opt('*', _) => (),
+            _ => unreachable!(),
+        }
+    }
+
+    opts.loose = opt.loose;
+    Ok(opts)
 }
 
 fn parse_size(size: u64) -> Result<String> {
